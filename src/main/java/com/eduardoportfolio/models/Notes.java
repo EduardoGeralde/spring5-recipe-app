650cc2c5cf90f4d48b5b48fdd4cc7f5024e75ca3
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-09 15:53:05 -0200

    Notes model created

---
+++ src/main/java/com/eduardoportfolio/models/Notes.java
@@ -0,0 +1,26 @@
+package com.eduardoportfolio.models;
+
+/**
+ * Created by Eduardo on 09/11/17.
+ */
+public class Notes {
+
+    private Recipe recipe;
+    private String recipeNotes;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
---
+++ src/main/java/com/eduardoportfolio/models/Recipe.java
@@ -0,0 +1,7 @@
+package com.eduardoportfolio.models;
+
+/**
+ * Created by Eduardo on 09/11/17.
+ */
+public class Recipe {
+}
