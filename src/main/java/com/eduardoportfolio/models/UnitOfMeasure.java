a1e964d393410ccc333ca026d72febb8d26ac4ac
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-09 16:58:09 -0200

    Unit of Measure class created

--- src/main/java/com/eduardoportfolio/models/UnitOfMeasure.java
+++ src/main/java/com/eduardoportfolio/models/UnitOfMeasure.java
@@ -1,7 +1,31 @@
 package com.eduardoportfolio.models;
 
+import javax.persistence.*;
+
 /**
  * Created by Eduardo on 09/11/17.
  */
+@Entity
 public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
 }
