897c4de7725e7864ebc55e50c60b29d8e9daca0d
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-10 17:53:49 -0200

    Category repo created

---
+++ src/main/java/com/eduardoportfolio/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.eduardoportfolio.repositories;
+
+import com.eduardoportfolio.models.Category;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Eduardo on 10/11/17.
+ */
+public interface CategoryRepository extends CrudRepository<Category, Long>{
+}
---
+++ src/main/java/com/eduardoportfolio/repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package com.eduardoportfolio.repositories;
+
+/**
+ * Created by Eduardo on 10/11/17.
+ */
+public interface RecipeRepository {
+}
