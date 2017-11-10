2331d7c50bf81b088a9d79e86c623842e96d5df7
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-10 17:54:31 -0200

    UnitOfMeasue repo created

--- src/main/java/com/eduardoportfolio/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/eduardoportfolio/repositories/UnitOfMeasureRepository.java
@@ -1,7 +1,10 @@
 package com.eduardoportfolio.repositories;
 
+import com.eduardoportfolio.models.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Eduardo on 10/11/17.
  */
-public interface UnitOfMeasureRepository {
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
 }
