f28701fa03181b5d3e350abaeab8d9d58b839385
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-10 17:54:08 -0200

    Recipe repo created

--- src/main/java/com/eduardoportfolio/repositories/RecipeRepository.java
+++ src/main/java/com/eduardoportfolio/repositories/RecipeRepository.java
@@ -1,7 +1,10 @@
 package com.eduardoportfolio.repositories;
 
+import com.eduardoportfolio.models.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Eduardo on 10/11/17.
  */
-public interface RecipeRepository {
+public interface RecipeRepository extends CrudRepository<Recipe,Long>{
 }
