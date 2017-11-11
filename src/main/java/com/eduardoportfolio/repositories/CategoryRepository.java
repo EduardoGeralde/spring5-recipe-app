c6f9b5ed929e9116fd1193552c7a5eb2c38fe039
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-11 08:56:30 -0200

    findByDescription query method created

--- src/main/java/com/eduardoportfolio/repositories/CategoryRepository.java
+++ src/main/java/com/eduardoportfolio/repositories/CategoryRepository.java
@@ -3,8 +3,13 @@ package com.eduardoportfolio.repositories;
 import com.eduardoportfolio.models.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
+
 /**
  * Created by Eduardo on 10/11/17.
  */
 public interface CategoryRepository extends CrudRepository<Category, Long>{
+
+    Optional <Category> findByDescription (String description);
 }
