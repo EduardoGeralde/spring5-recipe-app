2ac0aa98ebdeb8aa5b76435ed058f912e19d713f
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-11 08:56:15 -0200

    just configured to print the result from findByDescription

--- src/main/java/com/eduardoportfolio/controllers/IndexController.java
+++ src/main/java/com/eduardoportfolio/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package com.eduardoportfolio.controllers;
 
+import com.eduardoportfolio.models.Category;
+import com.eduardoportfolio.models.UnitOfMeasure;
+import com.eduardoportfolio.repositories.CategoryRepository;
+import com.eduardoportfolio.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by Eduardo on 08/11/17.
@@ -9,8 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    CategoryRepository categoryRepository;
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat ID is: " + categoryOptional.get().getId());
+        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
