35ec787e88ed83a3370a311ce5fa5b8c02f1386e
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-11 09:04:00 -0200

    findByDescription query method created

--- src/main/java/com/eduardoportfolio/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/eduardoportfolio/repositories/UnitOfMeasureRepository.java
@@ -3,8 +3,12 @@ package com.eduardoportfolio.repositories;
 import com.eduardoportfolio.models.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Eduardo on 10/11/17.
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
+
+    Optional <UnitOfMeasure> findByDescription (String description);
 }
