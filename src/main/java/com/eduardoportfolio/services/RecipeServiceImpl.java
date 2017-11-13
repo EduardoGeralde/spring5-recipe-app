b6cff3c4ea16c8dc2d95f1cc941cfd753aa88ab5
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-13 10:49:30 -0200

    recipe service implementation added

--- src/main/java/com/eduardoportfolio/services/RecipeServiceImpl.java
+++ src/main/java/com/eduardoportfolio/services/RecipeServiceImpl.java
@@ -1,7 +1,28 @@
 package com.eduardoportfolio.services;
 
+import com.eduardoportfolio.models.Recipe;
+import com.eduardoportfolio.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Eduardo on 11/11/17.
  */
-public class RecipeServiceImpl {
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
 }
