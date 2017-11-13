96aa00a85cec4ef88d08723a8c99799297556a3e
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-13 10:49:07 -0200

    recipe service interface added

--- src/main/java/com/eduardoportfolio/services/RecipeService.java
+++ src/main/java/com/eduardoportfolio/services/RecipeService.java
@@ -1,7 +1,13 @@
 package com.eduardoportfolio.services;
 
+import com.eduardoportfolio.models.Recipe;
+
+import java.util.Set;
+
 /**
  * Created by Eduardo on 11/11/17.
  */
 public interface RecipeService {
+
+    Set<Recipe> getRecipes();
 }
