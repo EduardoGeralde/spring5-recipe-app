db2dc86b7872426ce4951a48e113a8b4ed48104a
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-14 22:17:37 -0200

    index controller test created

---
+++ src/test/java/com/eduardoportfolio/models/CategoryTest.java
@@ -0,0 +1,26 @@
+package com.eduardoportfolio.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eduardo on 14/11/17.
+ */
+public class CategoryTest {
+    @Test
+    public void getId() throws Exception {
+
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/eduardoportfolio/services/RecipeServiceImplTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eduardo on 14/11/17.
+ */
+public class RecipeServiceImplTest {
+
+}
