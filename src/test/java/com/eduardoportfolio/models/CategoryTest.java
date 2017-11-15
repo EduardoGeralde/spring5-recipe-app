d7d7492ea02d96a78df56468e57cfbe11ed35e79
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-15 17:13:42 -0200

    Category Test created

--- src/test/java/com/eduardoportfolio/models/CategoryTest.java
+++ src/test/java/com/eduardoportfolio/models/CategoryTest.java
@@ -1,5 +1,6 @@
 package com.eduardoportfolio.models;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,20 @@ import static org.junit.Assert.*;
  * Created by Eduardo on 14/11/17.
  */
 public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() throws Exception {
+        category = new Category();
+
+    }
+
     @Test
     public void getId() throws Exception {
-
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
     }
 
     @Test
