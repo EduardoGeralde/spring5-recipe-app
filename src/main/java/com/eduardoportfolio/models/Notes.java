d3012b6837cf4f7d5d43aed2d368b6020bcda4bd
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-14 22:20:53 -0200

    project lombok added

--- src/main/java/com/eduardoportfolio/models/Notes.java
+++ src/main/java/com/eduardoportfolio/models/Notes.java
@@ -1,10 +1,14 @@
 package com.eduardoportfolio.models;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 /**
  * Created by Eduardo on 09/11/17.
  */
+@Data
+@EqualsAndHashCode (exclude = "recipe")
 @Entity
 public class Notes {
 
@@ -16,27 +20,4 @@ public class Notes {
     @Lob
     private String recipeNotes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
