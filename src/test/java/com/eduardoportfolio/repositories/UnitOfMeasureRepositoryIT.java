588b1a0bf6cadd6f0cf1fd53016681374fd3f080
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-15 12:31:45 -0200

    maven failsafe plug in added

---
+++ src/test/java/com/eduardoportfolio/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,22 @@
+package com.eduardoportfolio.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eduardo on 15/11/17.
+ */
+public class UnitOfMeasureRepositoryIT {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void findByDescription() throws Exception {
+
+    }
+
+}
