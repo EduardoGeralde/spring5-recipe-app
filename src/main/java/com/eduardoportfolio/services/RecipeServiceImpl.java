be6eb3ecc0062017f31dfa05007738fb2f97cadf
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-14 22:21:28 -0200

    project lombok log added

--- src/main/java/com/eduardoportfolio/services/RecipeServiceImpl.java
+++ src/main/java/com/eduardoportfolio/services/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package com.eduardoportfolio.services;
 
 import com.eduardoportfolio.models.Recipe;
 import com.eduardoportfolio.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Set;
 /**
  * Created by Eduardo on 11/11/17.
  */
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -21,6 +23,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("We are in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
