9bc2971a913d0f56cee3e560e67f7ae860672e69
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-15 22:28:16 -0200

    RecipeController just created to run a fail test TDD

--- src/main/java/com/eduardoportfolio/controllers/RecipeController.java
+++ src/main/java/com/eduardoportfolio/controllers/RecipeController.java
@@ -1,7 +1,19 @@
 package com.eduardoportfolio.controllers;
 
+import com.eduardoportfolio.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Eduardo on 15/11/17.
  */
+@Slf4j
+@Controller
 public class RecipeController {
+
+    RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 }
