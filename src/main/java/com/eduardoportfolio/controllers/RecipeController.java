e7b9175b9a62292cb713e0f083f1d9d85f5ca945
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-16 17:34:59 -0200

    bind and view path changed

--- src/main/java/com/eduardoportfolio/controllers/RecipeController.java
+++ src/main/java/com/eduardoportfolio/controllers/RecipeController.java
@@ -1,8 +1,13 @@
 package com.eduardoportfolio.controllers;
 
+import com.eduardoportfolio.models.Recipe;
 import com.eduardoportfolio.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Eduardo on 15/11/17.
@@ -13,7 +18,14 @@ public class RecipeController {
 
     RecipeService recipeService;
 
+    @Autowired
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/{id}")
+    public String showRecipeById(@PathVariable ("id") Long id, Model model){
+        model.addAttribute("recipe", recipeService.findById(id));
+        return "recipe/showRecipe";
     }
 }
