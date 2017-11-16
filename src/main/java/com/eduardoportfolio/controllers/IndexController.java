9beada69641a4eacb30ef6529ea94cf78fad99c3
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-15 22:27:44 -0200

    log message changed

--- src/main/java/com/eduardoportfolio/controllers/IndexController.java
+++ src/main/java/com/eduardoportfolio/controllers/IndexController.java
@@ -23,7 +23,7 @@ public class IndexController {
     @RequestMapping({"","/","index"})
     public String getIndexPage(Model model){
 
-        log.debug("Getting Index Page");
+        log.debug("IndexController getIndexPage");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
