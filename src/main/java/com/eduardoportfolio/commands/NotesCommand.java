7b20aaa31dc9be14f990f50b3a200a4e312cda83
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-18 10:30:39 -0200

    notes fields implemented

--- src/main/java/com/eduardoportfolio/commands/NotesCommand.java
+++ src/main/java/com/eduardoportfolio/commands/NotesCommand.java
@@ -1,7 +1,17 @@
 package com.eduardoportfolio.commands;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * Created by Eduardo on 18/11/17.
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class NotesCommand {
+
+    private Long id;
+    private String recipeNotes;
 }
