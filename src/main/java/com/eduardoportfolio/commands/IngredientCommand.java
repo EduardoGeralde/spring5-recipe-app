4e7787d940de7b5432d1c43caff3b35c397e6690
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-18 10:30:10 -0200

    igredient fields implemented

--- src/main/java/com/eduardoportfolio/commands/IngredientCommand.java
+++ src/main/java/com/eduardoportfolio/commands/IngredientCommand.java
@@ -1,7 +1,23 @@
 package com.eduardoportfolio.commands;
 
+import com.eduardoportfolio.models.UnitOfMeasure;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
 /**
  * Created by Eduardo on 18/11/17.
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class IngredientCommand {
+
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    private UnitOfMeasure unitOfMeasure;
+
 }
