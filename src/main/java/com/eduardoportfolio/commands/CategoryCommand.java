baf1403c8118553219df0e181b9d1851a6f25feb
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-18 10:29:38 -0200

    category implemented

--- src/main/java/com/eduardoportfolio/commands/CategoryCommand.java
+++ src/main/java/com/eduardoportfolio/commands/CategoryCommand.java
@@ -1,7 +1,19 @@
 package com.eduardoportfolio.commands;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
 /**
  * Created by Eduardo on 18/11/17.
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class CategoryCommand {
+
+    private Long id;
+    private String description;
 }
