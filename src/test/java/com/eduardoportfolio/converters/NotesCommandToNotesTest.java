efd17a10264ddb35d9e17dd3226e05d5aec4555f
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-19 12:32:48 -0200

    test created

---
+++ src/test/java/com/eduardoportfolio/converters/NotesCommandToNotesTest.java
@@ -0,0 +1,22 @@
+package com.eduardoportfolio.converters;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eduardo on 19/11/17.
+ */
+public class NotesCommandToNotesTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void convert() throws Exception {
+
+    }
+
+}
