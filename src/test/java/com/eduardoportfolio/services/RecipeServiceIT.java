dd9f3d56b27a465962079dc7e9c7278b985918ac
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-20 00:43:12 -0200

    Integration test added

--- src/test/java/com/eduardoportfolio/services/RecipeServiceIT.java
+++ src/test/java/com/eduardoportfolio/services/RecipeServiceIT.java
@@ -1,7 +1,53 @@
 package com.eduardoportfolio.services;
 
+import com.eduardoportfolio.commands.RecipeCommand;
+import com.eduardoportfolio.converters.RecipeCommandToRecipe;
+import com.eduardoportfolio.converters.RecipeToRecipeCommand;
+import com.eduardoportfolio.models.Recipe;
+import com.eduardoportfolio.repositories.RecipeRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
 /**
  * Created by Eduardo on 20/11/17.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class RecipeServiceIT {
+
+    public static final String NEW_DESCRIPTION = "New Decription";
+
+    @Autowired
+    RecipeService recipeService;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Autowired
+    RecipeCommandToRecipe recipeCommandToRecipe;
+
+    @Autowired
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Transactional
+    @Test
+    public void testSaveDescription(){
+
+        //given
+        Iterable<Recipe> recipes = recipeRepository.findAll();
+        Recipe testRecipe = recipes.iterator().next();
+        RecipeCommand testRecipeCommand = recipeToRecipeCommand.convert(testRecipe);
+
+        //when
+        testRecipeCommand.setDescription(NEW_DESCRIPTION);
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(testRecipeCommand);
+
+        //then
+        
+    }
 }
