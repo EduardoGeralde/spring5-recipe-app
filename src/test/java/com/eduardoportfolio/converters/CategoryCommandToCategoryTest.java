432131001730e4ac86e8f8ea43b0edce126f54d9
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-20 00:39:50 -0200

    test implemented

--- src/test/java/com/eduardoportfolio/converters/CategoryCommandToCategoryTest.java
+++ src/test/java/com/eduardoportfolio/converters/CategoryCommandToCategoryTest.java
@@ -1,5 +1,7 @@
 package com.eduardoportfolio.converters;
 
+import com.eduardoportfolio.commands.CategoryCommand;
+import com.eduardoportfolio.models.Category;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,14 +11,39 @@ import static org.junit.Assert.*;
  * Created by Eduardo on 19/11/17.
  */
 public class CategoryCommandToCategoryTest {
+    public static final Long ID_VALUE = new Long(1L);
+    public static final String DESCRIPTION = "description";
+    CategoryCommandToCategory converter;
+
     @Before
     public void setUp() throws Exception {
+        converter = new CategoryCommandToCategory();
+    }
 
+    @Test
+    public void testNullObject() throws Exception {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() throws Exception {
+        assertNotNull(converter.convert(new CategoryCommand()));
     }
 
     @Test
     public void convert() throws Exception {
 
-    }
+        //given
+        CategoryCommand categoryCommand = new CategoryCommand();
+        categoryCommand.setId(ID_VALUE);
+        categoryCommand.setDescription(DESCRIPTION);
 
+        //When
+        Category category = converter.convert(categoryCommand);
+
+        //then
+        assertNotNull(category);
+        assertEquals(ID_VALUE, category.getId());
+        assertEquals(DESCRIPTION, category.getDescription());
+    }
 }
---
+++ src/test/java/com/eduardoportfolio/services/RecipeServiceIT.java
@@ -0,0 +1,7 @@
+package com.eduardoportfolio.services;
+
+/**
+ * Created by Eduardo on 20/11/17.
+ */
+public class RecipeServiceIT {
+}
