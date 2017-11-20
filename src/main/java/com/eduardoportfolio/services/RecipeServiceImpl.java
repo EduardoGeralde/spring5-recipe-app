bcc3bb4255ae9c812600547f3a8203f38bdd2699
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-20 17:32:27 -0200

    Transactional javax changed to Transactioal for Spring

--- src/main/java/com/eduardoportfolio/services/RecipeServiceImpl.java
+++ src/main/java/com/eduardoportfolio/services/RecipeServiceImpl.java
@@ -7,8 +7,9 @@ import com.eduardoportfolio.models.Recipe;
 import com.eduardoportfolio.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
