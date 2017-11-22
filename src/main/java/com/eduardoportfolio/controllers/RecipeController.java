ed3834564f27812cf5a7345d2cba5e2422d39048
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-22 17:41:33 -0200

    recipeForm, updateRecipe, saveOrUpdate actions created, and showById  changed the bind

--- src/main/java/com/eduardoportfolio/controllers/RecipeController.java
+++ src/main/java/com/eduardoportfolio/controllers/RecipeController.java
@@ -1,13 +1,13 @@
 package com.eduardoportfolio.controllers;
 
+import com.eduardoportfolio.commands.RecipeCommand;
 import com.eduardoportfolio.models.Recipe;
 import com.eduardoportfolio.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Eduardo on 15/11/17.
@@ -16,16 +16,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RecipeController {
 
-    RecipeService recipeService;
+    private final RecipeService recipeService;
 
     @Autowired
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/{id}")
-    public String showRecipeById(@PathVariable ("id") Long id, Model model){
+    @RequestMapping("/recipe/{id}/show")
+    public String showById(@PathVariable ("id") Long id, Model model){
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/showRecipe";
+    }
+
+    @RequestMapping("recipe/new")
+    public String recipeForm(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeForm";
+    }
+
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable Long id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(id));
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/"+ savedCommand.getId()+ "/show";
     }
 }
