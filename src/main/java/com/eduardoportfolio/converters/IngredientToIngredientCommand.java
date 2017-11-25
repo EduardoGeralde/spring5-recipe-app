52c7ef5e33513852fb761dd1add29024cc47909c
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-25 15:29:20 -0200

    setting the recepeId property from the IngredientCommand class

--- src/main/java/com/eduardoportfolio/converters/IngredientToIngredientCommand.java
+++ src/main/java/com/eduardoportfolio/converters/IngredientToIngredientCommand.java
@@ -26,9 +26,11 @@ public class IngredientToIngredientCommand implements Converter<Ingredient,Ingre
         if(ingredient == null){
             return null;
         }
-
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
+        if (ingredient.getRecipe() != null) {
+            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+        }
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUnitOfMeasure(uomConverter.convert(ingredient.getUnitOfMeasure()));
