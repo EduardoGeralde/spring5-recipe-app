b0c0aecc888f08f55ddcda539f514f23b5680c4e
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-27 08:00:23 -0200

    Ingredient Service implementation created, saveIngredientCommand method created

--- src/main/java/com/eduardoportfolio/services/IngredientServiceImpl.java
+++ src/main/java/com/eduardoportfolio/services/IngredientServiceImpl.java
@@ -1,9 +1,12 @@
 package com.eduardoportfolio.services;
 
 import com.eduardoportfolio.commands.IngredientCommand;
+import com.eduardoportfolio.converters.IngredientCommandToIngredient;
 import com.eduardoportfolio.converters.IngredientToIngredientCommand;
+import com.eduardoportfolio.models.Ingredient;
 import com.eduardoportfolio.models.Recipe;
 import com.eduardoportfolio.repositories.RecipeRepository;
+import com.eduardoportfolio.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +20,18 @@ import java.util.Optional;
 public class IngredientServiceImpl implements IngredientService {
 
     private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final IngredientCommandToIngredient ingredientCommandToIngredient;
     private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand,
+                                 IngredientCommandToIngredient ingredientCommandToIngredient,
+                                 RecipeRepository recipeRepository,
+                                 UnitOfMeasureRepository unitOfMeasureRepository) {
         this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.ingredientCommandToIngredient = ingredientCommandToIngredient;
         this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
     @Override
@@ -45,5 +55,46 @@ public class IngredientServiceImpl implements IngredientService {
         }
 
         return ingredientCommandOptional.get();
+    }
+
+    @Override
+    public IngredientCommand saveIngredientCommand(IngredientCommand command) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(command.getRecipeId());
+
+        if(!recipeOptional.isPresent()){
+            log.error("Recipe Not Found, recipeID: "+command.getRecipeId());
+            return new IngredientCommand();
+        } else {
+            Recipe recipe = recipeOptional.get();
+
+            //Find if the Ingredient already exist in the recipe
+            Optional<Ingredient> ingredientOptional = recipe
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.getId().equals(command.getId()))
+                    .findFirst();
+
+            //If the Ingredient exists, just update the Ingredient with the parameter data.
+            if(ingredientOptional.isPresent()){
+                Ingredient ingredientFound = ingredientOptional.get();
+                ingredientFound.setDescription(command.getDescription());
+                ingredientFound.setAmount(command.getAmount());
+                ingredientFound.setUnitOfMeasure(unitOfMeasureRepository
+                        .findById(command.getUnitOfMeasure().getId())
+                        .orElseThrow(() -> new RuntimeException("UOM Not Found")));
+            } else {
+                //If not, add to the Recipe
+                recipe.addIngredient(ingredientCommandToIngredient.convert(command));
+            }
+
+            //Saving the Recipe with the Ingredient Data changed (New or Updated)
+            Recipe savedRecipe = recipeRepository.save(recipe);
+
+            //To do check for fail
+            return ingredientToIngredientCommand.convert(savedRecipe.getIngredients().stream()
+                    .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
+                    .findFirst()
+                    .get());
+        }
     }
 }
