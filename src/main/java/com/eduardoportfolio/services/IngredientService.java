64e238c5d20fa6be87e4cb10ca46abc505c78dbc
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-27 08:07:38 -0200

    saveIngredientCommand method added

--- src/main/java/com/eduardoportfolio/services/IngredientService.java
+++ src/main/java/com/eduardoportfolio/services/IngredientService.java
@@ -8,4 +8,5 @@ import com.eduardoportfolio.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
 }
