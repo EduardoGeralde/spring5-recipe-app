86cb3bbe57929a3ca814cfabf8431380e1316ee8
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-30 06:23:00 -0200

    removed ambiguous mapping

--- src/main/java/com/eduardoportfolio/controllers/RecipeController.java
+++ src/main/java/com/eduardoportfolio/controllers/RecipeController.java
@@ -1,6 +1,7 @@
 package com.eduardoportfolio.controllers;
 
 import com.eduardoportfolio.commands.RecipeCommand;
+import com.eduardoportfolio.models.Difficulty;
 import com.eduardoportfolio.models.Recipe;
 import com.eduardoportfolio.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
@@ -23,40 +24,33 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/show")
+    @GetMapping ("recipe/{id}/show")
     public String showById(@PathVariable ("id") Long id, Model model){
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/showRecipe";
     }
 
-    @GetMapping
-    @RequestMapping("recipe/new")
+    @GetMapping ("recipe/new")
     public String recipeForm(Model model){
         model.addAttribute("recipe", new RecipeCommand());
         return "recipe/recipeForm";
     }
 
-    @GetMapping
-    @RequestMapping("recipe/{id}/update")
+    @GetMapping ("recipe/{id}/update")
     public String updateRecipe(@PathVariable Long id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(id));
         return "recipe/recipeForm";
     }
 
-    @PostMapping
-    @RequestMapping("recipe")
+    @PostMapping ("recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
-
         return "redirect:/recipe/"+ savedCommand.getId()+ "/show";
     }
 
-    @GetMapping
-    @RequestMapping(value = "/recipe/{id}/delete")
+    @GetMapping ("recipe/{id}/delete")
     public String deleteById(@PathVariable("id")Long id){
         log.debug("Deleting Recipe ID: " + id);
-
         recipeService.deleteById(id);
         return "redirect:/";
     }
